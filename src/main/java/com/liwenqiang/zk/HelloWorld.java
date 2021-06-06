aeaf204367f9c7a0c807ed447dcacdf2ffd37371
李文强 <liwenqiang@cloudwalk.com>
2021-06-07 02:49:08 +0800

    add master and helloworld

--- src/main/java/com/liwenqiang/zk/HelloWorld.java
+++ src/main/java/com/liwenqiang/zk/HelloWorld.java
@@ -6,7 +6,14 @@ public class HelloWorld {
     public static void main(String[] args) throws IOException, InterruptedException {
         Master m = new Master(args[0]);
         m.startZK();
-        Thread.sleep(20000);
+        m.runForMaster();
+        if (m.isLeader) {
+            System.out.println("I'm the leader");
+            Thread.sleep(20000);
+        }else{
+            System.out.println("Someone else is the leader");
+        }
+
         m.stopZK();
     }
 }
--- src/main/java/com/liwenqiang/zk/Master.java
+++ src/main/java/com/liwenqiang/zk/Master.java
@@ -1,15 +1,16 @@
 package com.liwenqiang.zk;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class Master implements Watcher {
     ZooKeeper zk;
     String hostPort;
-
+    String serverId = Integer.toString(new Random().nextInt());
+    boolean isLeader = false;
     public Master(String hostPort) {
         this.hostPort = hostPort;
     }
@@ -24,5 +25,37 @@ public class Master implements Watcher {
 
     public void stopZK() throws InterruptedException {
         zk.close();
+    }
+
+    public boolean checkMaster() {
+        while (true) {
+            try {
+                Stat stat = new Stat();
+                byte[] data = zk.getData("/master", false, stat);
+                isLeader = new String(data).equals(serverId);
+                return true;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            } catch (KeeperException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void runForMaster() {
+        while (true) {
+            try {
+                zk.create("/master",serverId.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL);
+                isLeader = true;
+                break;
+            } catch (InterruptedException e) {
+                isLeader = false;
+                break;
+            } catch (KeeperException e) {
+                e.printStackTrace();
+            }
+            if (checkMaster()) break;
+        }
     }
 }
